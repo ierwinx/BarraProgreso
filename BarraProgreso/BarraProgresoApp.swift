63ade34ad92725000b9306111fa2c2b2cf446e85
Eяw!и Gontier <ierwinx@me.com>
2022-10-07 15:53:30 -0500

    Initial Commit

---
+++ BarraProgreso/BarraProgresoApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarraProgresoApp.swift
+//  BarraProgreso
+//
+//  Created by Erwin Luz León on 07/10/22.
+//
+
+import SwiftUI
+
+@main
+struct BarraProgresoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
