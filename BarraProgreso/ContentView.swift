1ab2eea94393ff969511414cd39642a57b5b5ce5
Eяw!и Gontier <ierwinx@me.com>
2022-10-07 16:21:14 -0500

    se agregan diferentes barras de progreso

--- BarraProgreso/ContentView.swift
+++ BarraProgreso/ContentView.swift
@@ -1,21 +1,90 @@
-//
-//  ContentView.swift
-//  BarraProgreso
-//
-//  Created by Erwin Luz León on 07/10/22.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var iValue: Double = 0.3
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Form {
+                Section("Linear") {
+                    Gauge(value: iValue) {
+                        Text("\(Int(iValue*100)) %")
+                    }
+                    
+                    Gauge(value: iValue) {
+                        Text("arriba \(Int(iValue*100)) %")
+                    } currentValueLabel: {
+                        Text("abajo \(Int(iValue*100)) %")
+                    }
+                    .tint(.green)
+                    
+                    Gauge(value: iValue) {
+                        Text("Barra")
+                    } currentValueLabel: {
+                        Text("\(Int(iValue*100)) %")
+                    }
+                    .gaugeStyle(.accessoryLinear)
+                    .tint(.red)
+                    
+                    Gauge(value: iValue) {
+                        Text("\(Int(iValue*100)) %")
+                    }
+                    .gaugeStyle(.accessoryLinearCapacity)
+                    .tint(.purple)
+                }
+                
+                Section("Circular") {
+                    HStack {
+                        Gauge(value: iValue) {
+                            Text("\(Int(iValue*100))%")
+                        }
+                        .gaugeStyle(.accessoryCircularCapacity)
+                        .tint(.red)
+                        
+                        Gauge(value: iValue) {
+                            Text("\(Int(iValue*100))%")
+                        }
+                        .gaugeStyle(.accessoryCircular)
+                        .tint(.red)
+                        
+                        Gauge(value: iValue) {
+                            Text("\(Int(iValue*100))%")
+                        }currentValueLabel: {
+                            Text("\(Int(iValue*100))%")
+                        }
+                        .gaugeStyle(.accessoryCircular)
+                        .tint(.red)
+                    }
+                    
+                    HStack {
+                        Gauge(value: iValue) {
+                            Image(systemName: "touchid", variableValue: iValue)
+                        }
+                        .gaugeStyle(.accessoryCircularCapacity)
+                        .tint(.purple)
+                        
+                        Gauge(value: iValue) {
+                            Image(systemName: "wifi", variableValue: iValue)
+                        }
+                        .gaugeStyle(.accessoryCircularCapacity)
+                        .tint(.purple)
+                        
+                        Gauge(value: iValue) {
+                            Text("Wifi")
+                        } currentValueLabel: {
+                            Image(systemName: "wifi", variableValue: iValue)
+                        }
+                        .gaugeStyle(.accessoryCircular)
+                        .tint(.cyan)
+                    }
+                    
+                }
+            }
+            
+            Slider(value: $iValue)
+                .padding(20)
         }
-        .padding()
     }
 }
 
